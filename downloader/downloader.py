a06ded9fc6a2324e336a21bacfe08011261b4012
crystal100794 <minhnn1007@gmail.com>
2019-08-21 11:41:14 +0700

    Add idea to gitignore

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,20 +1,21 @@
 import requests
 from lxml import html
 import wget
-
+import schedule
+import time
 # downloader is a function for downloading grib2 data from this url
 # https://nomads.ncdc.noaa.gov/data/gfs4/
 # for example download data at August 1, 2019, the format will be
 # ym = 201908, ymd = 20190801
 # data_path = local path of data
-def file_downloader(url, data_path, ym, ymd):
+def file_downloader(url, data_path, ym, ymd, current_time, set_time):
     url_full = url + '/' + ym + '/' + ymd + '/'
-
     print('Start getting files from url', url_full)
     request = requests.get(url_full)
     webpage = html.fromstring(request.content)
 
     file_list = webpage.xpath('//a[contains(@href, "grb2")]//text()')
+
 
     for file in file_list:
         total_files = len(file_list)
@@ -22,6 +23,10 @@ def file_downloader(url, data_path, ym, ymd):
         print('Start downloading file ' + str(file_list.index(file) + 1) + '/' + str(total_files), download_url)
 
         #wget download and show the download progress bar
-        result = wget.download(download_url, data_path + '/' + file)
+        if current_time == set_time:
+            result = wget.download(download_url, data_path + '/' + file)
 
-        print(' === Result:', result)
+            print(' === Result:', result)
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from osgeo.gdalconst import GA_ReadOnly
-
+from watcher import watcher
 from downloader import downloader
 from grabber import data_grabber as dg
+from watcher import watcher
+from time import gmtime, strftime
+import datetime
+
 
 url = 'https://nomads.ncdc.noaa.gov/data/gfs4'
 ym = '201908'
-ymd = '20190801'
+ymd = '20190802'
 data_path = './DATA'
 grib_path = "./DATA/gfs_4_20190801_0000_000.grb2"
 
-# downloader.file_downloader(url, data_path, ym, ymd)
+now = datetime.datetime.now()
+current_time = now.strftime("%H:%M")
+set_time =  "11:41"
+
+downloader.file_downloader(url, data_path, ym, ymd, current_time, set_time)
 
 dg.grab_grb2_data(grib_path, 1)
+
